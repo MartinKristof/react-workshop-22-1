311f6ee568dbbed560e9f3ee821edb1b107bdb5a
Martin Kristof <martin.kristof@heureka.cz>
2022-07-19 09:56:41 +0200

    Add Reactstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import styles from './App.module.css';
-import { Components } from './examples/components/Components';
 
-const App = () => (
-  <div className={styles.app}>
-    <header className={styles.appHeader}>
-      <section>
-        <h1>React workshop 1</h1>
-        <Components />
-      </section>
-    </header>
-  </div>
-);
+const App = () => <div />;
 
 export default App;
